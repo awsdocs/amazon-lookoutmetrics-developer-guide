53b8a0db4bcad318410096253f31c36137d86791
Michael Wunderlich <mwunderl@amazon.com>
2021-03-25 00:34:25 -0700

    backfill script

--- sample-apps/data-processor/5-backfill.sh
+++ sample-apps/data-processor/5-backfill.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -eo pipefail
+BUCKET=$(cat bucket-name.txt)
 python3 function/lambda_function.test.py TestFunction.test_backfill
+mkdir -p /tmp/backfill
+mv /tmp/kms-timeseries* /tmp/backfill
+aws s3 sync /tmp/backfill s3://$BUCKET/backtest/
